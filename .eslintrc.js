3795112602a42bb16df82fa385ef75755d2bedea
Jakob Høgenes <1014990+jakhog@users.noreply.github.com>
2020-07-16 10:44:48 +0200

    Allow RED as a parameter name (it is convention)

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,4 +9,19 @@ module.exports = {
         sourceType: 'module',
         tsconfigRootDir: __dirname,
     },
+    rules: {
+        '@typescript-eslint/naming-convention': [
+            'error',
+            { selector: 'typeLike', format: ['PascalCase'], filter: { regex: '^(__String|[A-Za-z]+_[A-Za-z]+)$', match: false } },
+            { selector: 'interface', format: ['PascalCase'], custom: { regex: '^I[A-Z]', match: true }, filter: { regex: '^I(Arguments|TextWriter|O([A-Z][a-z]+[A-Za-z]*)?)$', match: true } },
+            { selector: 'variable', format: ['camelCase', 'PascalCase', 'UPPER_CASE'], leadingUnderscore: 'allow', filter: { regex: '^(_{1,2}filename|_{1,2}dirname|_+|[A-Za-z]+_[A-Za-z]+)$', match: false } },
+            { selector: 'function', format: ['camelCase', 'PascalCase'], leadingUnderscore: 'allow', filter: { regex: '^[A-Za-z]+_[A-Za-z]+$', match: false } },
+            { selector: 'parameter', format: ['camelCase'], leadingUnderscore: 'allow', filter: { regex: '^(_+|[A-Za-z]+_[A-Z][a-z]+)|RED$', match: false } },
+            { selector: 'method', format: ['camelCase', 'PascalCase'], leadingUnderscore: 'allow', filter: { regex: '^[A-Za-z]+_[A-Za-z]+$', match: false } },
+            { selector: 'memberLike', format: ['camelCase'], leadingUnderscore: 'allow', filter: { regex: '^[A-Za-z]+_[A-Za-z]+$', match: false } },
+            { selector: 'enumMember', format: ['camelCase', 'PascalCase'], leadingUnderscore: 'allow', filter: { regex: '^[A-Za-z]+_[A-Za-z]+$', match: false } },
+            // eslint-disable-next-line no-null/no-null
+            { selector: 'property', format: null }
+        ]
+    }
 };
