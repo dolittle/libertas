403c229c1378579c4df13d9ea628a9f01e9e83b9
Jakob Høgenes <1014990+jakhog@users.noreply.github.com>
2020-07-16 10:33:03 +0200

    Comment out libertas/mapping (needs reimplementation)

--- Source/libertas/mapping/PropertyMap.ts
+++ Source/libertas/mapping/PropertyMap.ts
@@ -1,11 +1,11 @@
 // Copyright (c) Dolittle. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-import { Property } from '../entities';
-import { PropertyDescriptor } from '@dolittle/rudiments';
+// import { Property } from '../entities';
+// import { PropertyDescriptor } from '@dolittle/rudiments';
 
-export class PropertyMap {
+// export class PropertyMap {
 
-    constructor(readonly property: Property, readonly propertyDescriptor: PropertyDescriptor) {
-    }
-}
+//     constructor(readonly property: Property, readonly propertyDescriptor: PropertyDescriptor) {
+//     }
+// }
